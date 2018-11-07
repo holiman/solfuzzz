9879766cdafca8069325ef9c2d988fb80f988197
Martin Holst Swende <martin@swende.se>
2018-11-07 14:59:16 +0100

    remove bad corpus files

--- corpus/solc/035_structs_in_libraries.sol
+++
@@ -1,7 +0,0 @@
-pragma experimental ABIEncoderV2;
-library L {
-    struct S { uint a; T[] sub; bytes b; }
-    struct T { uint[2] x; }
-    function f(L.S storage s) public {}
-    function g(L.S memory s) public {}
-}
--- corpus/solc/524_bare_call_invalid_address.sol
+++
@@ -1,5 +0,0 @@
-    contract C {
-        function f() external returns (bool) {
-            return address(0x4242).staticcall("");
-        }
-    }
